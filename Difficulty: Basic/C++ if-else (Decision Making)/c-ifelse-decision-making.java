2681e8e3d8dbac600734a891ee19e481625cbfd2
hitanshuagrawal <hitanshusanjeevagrawal@gmail.com>
2025-07-31 22:36:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/C++ if-else (Decision Making)/c-ifelse-decision-making.java
@@ -0,0 +1,11 @@
+class Solution {
+    static String compareFive(int n) {
+        if (n > 5) {
+            return "Greater than 5";
+        }
+        if (n < 5) {
+            return "Less than 5";
+        }
+        return "Equal to 5";
+    }
+}
